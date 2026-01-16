da503e2c45e066ac6bb5e7f02db84eba02f1ed5a
z.mark <2006zmark@gmail.com>
2026-01-16 09:16:34 +0100

    Lapok+Navbar letrehozasa

--- SzallasFoglalo.php
+++ SzallasFoglalo.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" href="stilus.css">
 </head>
 <body>
-    
+    <nav>
+        <h2>Hunguest Hotel</h2>
+        <a href="SzallasFoglalo.php">Kezdőlap</a>
+        <a href="Szobak.php">Szobák</a>
+        <a href="Foglalas.php">Foglalás</a>
+        <a href="Elerhetoseg.php">Elérhetőség</a>
+        <a href="Bejelentkezes.php">Bejelentkezés</a>
+    </nav>
 </body>
 </html>
