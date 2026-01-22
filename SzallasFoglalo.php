002a6c4bbcd76e21670c726f2ca89dbfd80af35e
Zombori Márk <2006zmark@gmail.com>
2026-01-22 09:55:07 +0100

    Badgek hozzaadasa

--- SzallasFoglalo.php
+++ SzallasFoglalo.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hunguest Hotel</title>
     <link rel="stylesheet" href="stilus.css">
+    <link rel="icon" href="img/HunguestLogo2.png">
 </head>
 <body>
     <nav class="Fejlec">
         <h2>Hunguest Hotel</h2>
+        <img src="img/HunguestLogo2.png" alt="HunguestLogo" id="nav-logo">
         <div class="nav-oldalak">
             <a href="SzallasFoglalo.php">Kezdőlap</a>
             <a href="Szobak.php">Szobák</a>
@@ -21,9 +23,45 @@
     <div class="hero">
         <div class="hero-text">
             <h1>Hunguest - pihenés, élmény, kényelem</h1>
-            <p>Üdvözöljük a Hunguest Hotel hivatalos oldalán! Fedezze fel szállodáink széles választékát, ahol a kényelem és a minőségi szolgáltatások találkoznak. Legyen szó családi nyaralásról, romantikus hétvégéről vagy üzleti útról, nálunk minden igényt kielégítünk.</p>
-            <p>Szobáink modern berendezéssel, ingyenes Wi-Fi-vel és lenyűgöző kilátással várják vendégeinket. Élvezze wellness központjaink szolgáltatásait, kóstolja meg éttermeink ínyenc fogásait.</p>
+            <p>Üdvözöljük a Hunguest Hotel hivatalos oldalán! <br> Fedezze fel szállodáink széles választékát, ahol a kényelem és a minőségi szolgáltatások találkoznak. <br> Legyen szó családi nyaralásról, romantikus hétvégéről vagy üzleti útról, nálunk minden igényt kielégítünk.</p>
+            <p>Szobáink modern berendezéssel, ingyenes Wi-Fi-vel és lenyűgöző kilátással várják vendégeinket. <br> Élvezze wellness központjaink szolgáltatásait, kóstolja meg éttermeink ínyenc fogásait.</p>
         </div>
     </div>
+
+    <section class="info-grid">
+    <div class="card">
+        <span class="badge">⭐ Prémium</span>
+        <h3>Rugalmas lemondás</h3>
+        <p>Érkezés előtt 48 órával ingyen módosítható.</p>
+    </div>
+
+    <div class="card">
+        <span class="badge">🧖 Spa</span>
+        <h3>Teljes körű wellness</h3>
+        <p>Szaunavilág, termál és kezelések - képekkel, csomagokkal.</p>
+    </div>
+
+    <div class="card">
+        <span class="badge">🚗 Parkolás</span>
+        <h3>Saját mélygarázs</h3>
+        <p>Őrzött parkoló közvetlen lifttel a lobbyba.</p>
+    </div>
+
+    <div class="card small">
+        <span class="badge">Telefon:</span> 
+        <p>+36 1 234 5678</p>
+    </div>
+
+    <div class="card small">
+        <span class="badge">E-mail:</span> 
+        <p>recepcio@hunguest.example</p>
+    </div>
+
+    <div class="card small">
+        <span class="badge">Cím:</span>
+        <p>1051 Budapest, Fő utca 1.</p> 
+    </div>
+</section>
+
 </body>
 </html>
